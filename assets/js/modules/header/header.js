3efe91111d4f5259e590ca4ac435efb75e2c17e0
enotstitch <kessler.anna96@gmail.com>
2026-07-14 03:03:56 +0300

    Update 2026-07-14T00:03:41.984Z

--- assets/js/modules/header/header.js
+++
@@ -1,9 +0,0 @@
-import { initBurgerMenu } from './burgerMenu.js';
-import { initMenuScroll } from './menuScroll.js';
-import { initStickyHeader } from './stickyHeader.js';
-
-export const initHeader = () => {
-	initBurgerMenu();
-	initMenuScroll();
-	initStickyHeader();
-};
